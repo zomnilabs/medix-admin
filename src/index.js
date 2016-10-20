f6d2b99213a05637b97c6ac5ef3886c581722e81
Alleo Indong <freedom_war550@live.com.ph>
2016-10-20 13:33:50 +0800

    added more dependencies

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import App from './App';
 import './index.css';
 import './flexboxgrid.css';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 ReactDOM.render(
   <App />,
