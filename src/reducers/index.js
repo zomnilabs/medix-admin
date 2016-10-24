5958f5f8d7ce817c6d5ed093fb05202ccf63579e
Alleo Indong <freedom_war550@live.com.ph>
2016-10-24 13:03:56 +0800

    fixed root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,9 @@
 import session from './session';
 import { routerReducer as routing } from 'react-router-redux';
-import { combineReducers } from 'redux';
 
-const rootReducer = combineReducers({
+const rootReducer = {
     session,
     routing
-});
+};
 
 export default rootReducer;
