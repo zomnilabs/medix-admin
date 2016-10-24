dd00e7d4cefafd473214aba0756ab98cd224acb1
Alleo Indong <freedom_war550@live.com.ph>
2016-10-24 13:03:30 +0800

    updated configurestore dev and prod

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -1,14 +1,20 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import createLogger from 'redux-logger';
 import rootReducer from '../reducers';
+import { reducer as formReducer } from 'redux-form';
+import { routerMiddleware } from 'react-router-redux';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const configureStore = preloadedState => {
     const store = createStore(
-        rootReducer,
-        preloadedState,
+        combineReducers({
+            ...rootReducer,
+            ...preloadedState,
+            form: formReducer,
+            routing: routerMiddleware
+        }),
         composeEnhancers(
             applyMiddleware(thunk, createLogger())
         )
--- src/store/configureStore.prod.js
+++ src/store/configureStore.prod.js
@@ -1,10 +1,16 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from '../reducers'
+import { reducer as formReducer } from 'redux-form';
+import { routerMiddleware } from 'react-router-redux';
 
 const configureStore = preloadedState => createStore(
-    rootReducer,
-    preloadedState,
+    combineReducers({
+        ...rootReducer,
+        ...preloadedState,
+        form: formReducer,
+        routing: routerMiddleware
+    }),
     applyMiddleware(thunk)
 );
 
